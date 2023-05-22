4b1fd622f84f862c8c85f1224098823fcc802c46
stevex626 <stevenx2021@gmail.com>
2023-05-21 20:28:07 -0700

    Updated login and home page display, update notes display

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -12,8 +14,28 @@ def create_app():
 
 
     from .views import views
-    from.auth import auth
+    from .auth import auth
     app.register_blueprint(views,url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Note
+
+
+    create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+    
     return app
+
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        with app.app_context():
+            db.create_all()
+        print('Database Created!')
